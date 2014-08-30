c3b19bdce33da44dae1fe5d975c67eda40866505
dtsoftweb224 <dtsoftweb@yandex.ru>
2014-08-30 19:57:41 +0600

    фыв

---
+++ sql/client.sql
@@ -0,0 +1,14 @@
+CREATE TABLE client (
+  id int(11) NOT NULL,
+  mail varchar(255) NOT NULL,
+  telephone varchar(12) DEFAULT NULL,
+  serial decimal(10, 0) NOT NULL,
+  number decimal(10, 0) NOT NULL,
+  fio varchar(255) NOT NULL,
+  address varchar(255) DEFAULT NULL,
+  PRIMARY KEY (mail)
+)
+ENGINE = INNODB
+AVG_ROW_LENGTH = 16384
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
